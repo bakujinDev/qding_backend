f4ff697a87deae4f485b38b50e4076a66702ebc6
bak-u-jin <87050600+bakujinDev@users.noreply.github.com>
2023-01-31 20:49:30 +0900

    create basic email post
    when err not found module environ
    pip uninstall / intstall django-environ

--- qding/settings.py
+++ qding/settings.py
@@ -11,16 +11,20 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-t3pd*aj9)&=r)f8x7+c9zb)5486ys2cv(7zu6_9l%^*ineosry"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -158,3 +162,11 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CORS_ALLOW_CREDENTIALS = True
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "qding.office@gmail.com"
+EMAIL_HOST_PASSWORD = env("GOOGLE_APP_PW")
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- users/views.py
+++ users/views.py
@@ -8,6 +8,34 @@ from rest_framework.permissions import IsAuthenticated
 from .models import User
 from .serializers import PrivateUserSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
+from django.core.mail import EmailMessage
+
+
+class User(APIView):
+    def post(self, request):
+        password = request.data.get("password")
+
+        if not password:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        serializer = PrivateUserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            print("able")
+            # user = serializer.save()
+            # user.set_password(password)
+            # user.save()
+            # serializer = PrivateUserSerializer(user)
+            # return Response(serializer.data)
+
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
 
 class Me(APIView):
@@ -39,4 +67,17 @@ class Logout(APIView):
         refresh = request.data.get("refresh")
         token = RefreshToken(refresh)
         token.blacklist()
+        return Response(status=status.HTTP_200_OK)
+
+
+class Email_Auth(APIView):
+    def post(self, request):
+        email_address = request.data.get("email_address")
+
+        email = EmailMessage(
+            "Title",  # 이메일 제목
+            "Content",  # 내용
+            to=[email_address],  # 받는 이메일
+        )
+        email.send()
         return Response(status=status.HTTP_200_OK)
