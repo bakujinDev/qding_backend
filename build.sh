918086a6344b7ed92e5aae41d262e90cddcdfef4
bak-u-jin <87050600+bakujinDev@users.noreply.github.com>
2023-03-01 00:11:22 +0900

    create django-cors-headers

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ pip install django-environ
 pip install dj-database-url
 pip install djangorestframework
 pip install djangorestframework-simplejwt
+pip install django-cors-headers
 
 python manage.py collectstatic --no-input
 python manage.py migrate
