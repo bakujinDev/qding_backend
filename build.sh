07074a590b7a918abbd775ede1f66bd8ff169526
bak-u-jin <87050600+bakujinDev@users.noreply.github.com>
2023-02-28 23:32:52 +0900

    patch build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 set -o errexit
 pip install --upgrade pip
 
-# poetry install
-# pip install --force-reinstall -U setuptools
+poetry install
+pip install --force-reinstall -U setuptools
 
 python manage.py collectstatic --no-input
 python manage.py migrate
