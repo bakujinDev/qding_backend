9f60af2795e349e4901af7084137c312f731947e
bak-u-jin <87050600+bakujinDev@users.noreply.github.com>
2023-03-01 00:08:11 +0900

    try pip install

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@
 set -o errexit
 pip install --upgrade pip
 
-poetry self update
-poetry lock
-poetry install
-# pip install django
-# pip install django-environ
-# pip install dj-database-url
-# pip install djangorestframework
-# pip install djangorestframework-simplejwt
+# poetry self update
+# poetry lock
+# poetry install
+pip install django
+pip install django-environ
+pip install dj-database-url
+pip install djangorestframework
+pip install djangorestframework-simplejwt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
