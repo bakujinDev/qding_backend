d6ed221f3d3275cc5dbddbcd6cf6d61b6dd3b905
bak-u-jin <87050600+bakujinDev@users.noreply.github.com>
2023-02-28 23:59:54 +0900

    add pip i djangorestframework

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ pip install --upgrade pip
 pip install django
 pip install django-environ
 pip install dj-database-url
+pip install djangorestframework
 
 python manage.py collectstatic --no-input
 python manage.py migrate
